41009e5c3295397939be87afece82cdf68462f7f
Kajal <57581853+GitHub-555@users.noreply.github.com>
2023-01-08 14:28:01 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,28 @@
+// var randomNumber1;
+// randomNumber1 = Math.floor(Math.random() * 6) + 1;
+// var randomImage = "dice" + randomNumber1 + ".png";
+// var randomImageSrc = randomImage;
+// var image1 = document.querySelectorAll("img")[0];
+// image1.setAttribute("src", randomImageSrc);
+
+// var randomNumber2;
+// randomNumber2 = Math.floor(Math.random() * 6) + 1;
+// var randomImage = "dice" + randomNumber2 + ".png";
+// var randomImageSrc = randomImage;
+// var image2 = document.querySelectorAll("img")[1];
+// image2.setAttribute("src", randomImageSrc);
+
+
 var randomNumber1;
 randomNumber1 = Math.floor(Math.random() * 6) + 1;
 var randomImage = "dice" + randomNumber1 + ".png";
-var randomImageSrc = randomImage;
-var image1 = document.querySelectorAll("img")[0];
-image1.setAttribute("src", randomImageSrc);
+document.querySelectorAll("img")[0].setAttribute("src", randomImage);
 
 var randomNumber2;
 randomNumber2 = Math.floor(Math.random() * 6) + 1;
 var randomImage = "dice" + randomNumber2 + ".png";
-var randomImageSrc = randomImage;
-var image2 = document.querySelectorAll("img")[1];
-image2.setAttribute("src", randomImageSrc);
+document.querySelectorAll("img")[1].setAttribute("src", randomImage);
+
 
 if (randomNumber1 > randomNumber2) {
   document.querySelector("h1").innerHTML = "Player 1 Won!!";
