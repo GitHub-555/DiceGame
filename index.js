8b61d4a4d0ddaaa82a45f9fed2172c1d3b44df70
Kajal <57581853+GitHub-555@users.noreply.github.com>
2023-01-08 14:06:27 +0530

    Files Added

---
+++ index.js
@@ -0,0 +1,21 @@
+var randomNumber1;
+randomNumber1 = Math.floor(Math.random() * 6) + 1;
+var randomImage = "dice" + randomNumber1 + ".png";
+var randomImageSrc = "images/" + randomImage;
+var image1 = document.querySelectorAll("img")[0];
+image1.setAttribute("src", randomImageSrc);
+
+var randomNumber2;
+randomNumber2 = Math.floor(Math.random() * 6) + 1;
+var randomImage = "dice" + randomNumber2 + ".png";
+var randomImageSrc = "images/" + randomImage;
+var image2 = document.querySelectorAll("img")[1];
+image2.setAttribute("src", randomImageSrc);
+
+if (randomNumber1 > randomNumber2) {
+  document.querySelector("h1").innerHTML = "Player 1 Won!!";
+} else if (randomNumber1 < randomNumber2) {
+  document.querySelector("h1").innerHTML = "Player 2 Won!!";
+} else {
+  document.querySelector("h1").innerHTML = "Draw!";
+}
