91a0093728866475ea7985b928e1eaa9954af60b
Kajal <57581853+GitHub-555@users.noreply.github.com>
2023-01-08 14:22:51 +0530

    added images to root.

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 var randomNumber1;
 randomNumber1 = Math.floor(Math.random() * 6) + 1;
 var randomImage = "dice" + randomNumber1 + ".png";
-var randomImageSrc = "images/" + randomImage;
+var randomImageSrc = randomImage;
 var image1 = document.querySelectorAll("img")[0];
 image1.setAttribute("src", randomImageSrc);
 
 var randomNumber2;
 randomNumber2 = Math.floor(Math.random() * 6) + 1;
 var randomImage = "dice" + randomNumber2 + ".png";
-var randomImageSrc = "images/" + randomImage;
+var randomImageSrc = randomImage;
 var image2 = document.querySelectorAll("img")[1];
 image2.setAttribute("src", randomImageSrc);
 
